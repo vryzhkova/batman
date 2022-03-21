80e02fee2dce190141f65b50551699fb09e80185
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-03-21 16:22:28 +0400

    added slider

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 3,
+    centeredSlides: true,
+    loopedSlides: 4,
+});
+
+sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.cliickedIndex)
+})
+
+const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    spaceBetween: 10,
+    loopedSlides: 4,
+});
+
+sliderThumbs.controller.control = sliderMain;
+sliderMain.controller.control = sliderThumbs;
